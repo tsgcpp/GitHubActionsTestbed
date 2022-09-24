7cb23a74c9094bba9825a07e8f4b1c7bb2f538e0
tsgcpp <tsgcpp@users.noreply.github.com>
2022-09-24 14:14:22 +0900

    chore: Add a note in IRandomizer

--- Lottery/IRandomizer.cs
+++ Lottery/IRandomizer.cs
@@ -2,6 +2,9 @@
 {
     public interface IRandomizer
     {
+        /// <summary>
+        /// Returns a random floating-point number that is greater than or equal to 0.0, and less than 1.0.
+        /// </summary>
         float Random { get; }
     }
 }
